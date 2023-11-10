e6da521784b9eff46267067923b01573eea01e21
Sergey Lutsenko <lytsenko.89@gmail.com>
2023-11-10 19:29:44 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import css from './App.module.css';
 import { NavLink, Navigate, Route, Routes } from 'react-router-dom';
-import { Suspense, lazy } from 'react';
-import { Loader } from './Loader/Loader';
+// import { Suspense, lazy } from 'react';
+// import { Loader } from './Loader/Loader';
+import Home from 'pages/Home/Home';
+import Movies from 'pages/Movies/Movies';
+import MovieDetails from 'pages/MovieDetails/MovieDetails';
 
-const Home = lazy(() => import('pages/Home/Home'));
-const Movies = lazy(() => import('pages/Movies/Movies'));
-const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+// const Home = lazy(() => import('pages/Home/Home'));
+// const Movies = lazy(() => import('pages/Movies/Movies'));
+// const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
 
 export const App = () => {
   return (
@@ -20,15 +23,14 @@ export const App = () => {
       </header>
 
       <main>
-        <Suspense fallback={<Loader />}>
-          {' '}
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/movies/:movieId/*" element={<MovieDetails />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>{' '}
-        </Suspense>
+        {/* <Suspense fallback={<Loader />}> */}{' '}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId/*" element={<MovieDetails />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>{' '}
+        {/* </Suspense> */}
       </main>
     </div>
   );
